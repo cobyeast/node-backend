2c0cda665ef6c06a64495a4f0b3c163a509df070
Coby Eastwood <cobyeastwood@cobys-mbp.home>
2020-07-23 22:17:41 -0700

    small cleanup recommit

--- helper.ts
+++ helper.ts
@@ -5,7 +5,7 @@ function trimObj(obj:any) {
   return Object.keys(obj).reduce((ths:any, nxt:any) => {
     ths[nxt] = obj[nxt].trim()
     return ths;
-  }, []);
+  }, {});
 }
 
 export default trimObj;
