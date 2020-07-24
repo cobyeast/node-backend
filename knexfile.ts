34b416dbe868b962e784d94c8fc50bd4ed0118aa
Coby Eastwood <cobyeastwood@cobys-mbp.home>
2020-07-24 15:51:32 -0700

    git ignore DS Store

--- knexfile.ts
+++ knexfile.ts
@@ -1,16 +1,15 @@
 // Setting up config
 
 module.exports = {
-
   development: {
     client: 'pg',
     connection: 'postgres://localhost/foundationdb',
     migrations: {
-      directory: __dirname + '/db/migrations'
+      directory: __dirname + '/db/migrations',
     },
     seeds: {
-      directory: __dirname + '/db/seeds/development'
-    }
+      directory: __dirname + '/db/seeds/development',
+    },
   },
 
   production: {
@@ -18,14 +17,13 @@ module.exports = {
     connection: 'postgres://localhost/foundationdb',
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      directory: __dirname + '/db/migrations'
+      directory: __dirname + '/db/migrations',
     },
     seeds: {
-      directory: __dirname + '/db/seeds/production'
-    }
-  }
-
+      directory: __dirname + '/db/seeds/production',
+    },
+  },
 };
